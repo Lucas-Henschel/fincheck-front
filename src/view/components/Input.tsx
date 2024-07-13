f5678857ed477be51b847f72851cd5fb2724234d
Lucas Gabriel <lucas101henschel@gmail.com>
2024-07-12 21:51:08 -0300

    Implemented login and signup windows

--- src/view/components/Input.tsx
+++ src/view/components/Input.tsx
@@ -13,12 +13,14 @@ export function Input({ placeholder, name, id, ...props }: InputProps) {
         {...props}
         name={name}
         id={inputId}
-        className="bg-white rounded-lg w-full border border-gray-500 px-3 h-[52px] text-gray-800"
+        className="bg-white rounded-lg w-full border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none"
+        placeholder=" "
       />
 
       <label
         htmlFor={inputId}
-        className="absolute left-[13px] top-3.5 pointer-events-none text-gray-700"
+        className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700
+        peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
       >
         { placeholder }
       </label>
