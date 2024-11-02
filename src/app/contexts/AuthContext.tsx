77cf93e4cf9b20833e49a443a92e0c8e623e718f
Lucas Gabriel <lucas101henschel@gmail.com>
2024-11-01 22:50:19 -0300

    Implementing Authentication Flow

--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -1,14 +1,26 @@
-import { createContext } from "react";
+import { createContext, useCallback, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
 
 export interface AuthContextValue {
   signedIn: boolean;
+  signin(accessToken: string): void;
 }
 
 export const AuthContext = createContext({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storageAccessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+    return !!storageAccessToken;
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+    setSignedIn(true);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signedIn: true }}>
+    <AuthContext.Provider value={{ signedIn, signin }}>
       { children }
     </AuthContext.Provider>
   );
--- src/view/pages/Login/useLoginController.ts
+++ src/view/pages/Login/useLoginController.ts
@@ -5,6 +5,7 @@ import { authService } from "../../../app/services/authService";
 import { useMutation } from "@tanstack/react-query";
 import { SigninṔarams } from "../../../app/services/authService/signin";
 import toast from "react-hot-toast";
+import { useAuth } from "../../../app/hooks/useAuth";
 
 const schema = z.object({
   email:
@@ -34,9 +35,13 @@ export function useLoginController() {
     },
   });
 
+  const { signin } = useAuth();
+
   const handleSubmit = hookFormSubmit(async (data) => {
     try {
-      await mutateAsync(data);
+      const { accessToken } = await mutateAsync(data);
+
+      signin(accessToken);
     } catch {
       toast.error("Credenciais invállidas!");
     }
